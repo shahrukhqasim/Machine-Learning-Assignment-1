7f4fef10ddd4df616374cec93c2c153b2e357355
Shah Rukh Qasim <14beesqasim@seecs.edu.pk>
2017-03-17 23:01:35 +0500

    fix: Fix formula for computation of mcorrcoef
    For computation of correlation coefficient, standard deviation is used. The formula for standard deviation was buggy. Subtration was mean was not being done. Fixed now.

--- mcorrcoef.m
+++ mcorrcoef.m
@@ -1,7 +1,7 @@
 function c=mcorrcoef(X,Y)
 
-sx=sqrt(sum(X.^2)/(length(X)-1));
-sy=sqrt(sum(Y.^2)/(length(Y)-1));
+sx=sqrt(sum((X-sum(X)/length(X)).^2)/(length(X)-1));
+sy=sqrt(sum((Y-sum(Y)/length(Y)).^2)/(length(Y)-1));
 
 c=mcovariance(X,Y)/(sx*sy);
 
